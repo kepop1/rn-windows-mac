06543ebfc5c36366ecdadf75fd9c894e6552cb3e
kepop1 <kepop123@gmail.com>
2020-08-23 11:20:23 +0100

    Remove React-Navigation as React-Native-Mac is not compatible.

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
+See [FORKED_README]('./FORKED_README.md') for information about the project's setup and structure.
+
 Mac:
 `npx react-native init <projectName> --version 0.61.5`
 
 `npx react-native-macos-init`
 
 `npx react-native run-macos`
+
+Windows:
+`npx react-native-windows-init --overwrite`
+
+`npx react-native run-windows`
+
+FluentUI
+
+`npm i @fluentui/react-native`
+
+Fluent UI Components Available: `https://developer.microsoft.com/en-us/fluentui#/controls/crossplatform`
+
+React-Navigation does not work with React-Native-Windows/Mac. That's a real kicker. Will try React-Router-DOM
+
+Backing out the changes for React Navigation caused a mess, if the react-native-macos cli bombs out it'll leave everything half setup - multiple git backouts during removal of react-navigation.
+
+Thankfully we should be good to go.
+
+I wonder what happens with fonts and assets?
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import 'react-native-gesture-handler' //Required for React-Navigation >v5 to be at the top
 import React from 'react'
-import { Text, StatusBar } from 'react-native'
+import { View, Text, StatusBar, SafeAreaView } from 'react-native'
 import { Provider } from 'react-redux'
-import { NavigationContainer } from '@react-navigation/native'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
 import store from './store'
-import RootNavigator from './navigation/RootNavigator'
 
 //This disables any of the Text from scaling according to the OS settings for text.
 //This is not a good idea long term and should be addressed!
@@ -16,11 +12,11 @@ Text.defaultProps = {
 
 export default () => (
   <Provider store={store}>
-    <NavigationContainer>
-      <SafeAreaProvider>
-        <StatusBar barStyle="light-content" />
-        <RootNavigator />
-      </SafeAreaProvider>
-    </NavigationContainer>
+    <SafeAreaView>
+      <StatusBar barStyle="light-content" />
+      <View style={{ backgroundColor: 'white', height: '100%' }}>
+        <Text>Testing...</Text>
+      </View>
+    </SafeAreaView>
   </Provider>
 )
